befb8cf280ce795e549d22cf850394cd5153f4f4
Yago-Maia <yagogmaia@gmail.com>
2019-04-12 11:13:41 -0300

    Página curso atualizada com as imagens de cada curso.

--- simplemoc/simplemoc/courses/views.py
+++ simplemoc/simplemoc/courses/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Course
+
 def index(request):
+    courses = Course.objects.all()
     template_name = 'courses/index.html'
-    return render(request, template_name)
+    context = {
+        'courses': courses
+    }
+    return render(request, template_name, context)
--- simplemoc/simplemoc/urls.py
+++ simplemoc/simplemoc/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include(('polls.urls', 'polls'), namespace='polls')),
     path('admin/', admin.site.urls),
     path('cursos/', include(('simplemoc.courses.urls', 'courses'), namespace='courses')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
